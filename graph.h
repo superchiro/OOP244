f258344aeb9a4170a01b0c3ef0e4769132018d7a
superchiro <85416957+superchiro@users.noreply.github.com>
2022-03-09 22:16:32 -0500

    Add files via upload

---
+++ graph.h
@@ -0,0 +1,17 @@
+
+
+#ifndef PROJECT_GRAPH_H
+#define PROJECT_GRAPH_H
+
+#endif //PROJECT_GRAPH_H
+#define MAX_NO_OF_SAMPLES 20
+
+namespace sict {
+    void getSamples(int samples[], int noOfSamples);
+
+    int findMax(int samples[], int noOfSamples);
+
+    void printBar(int val, int max);
+
+    void printGraph(int samples[], int noOfSamples);
+}
